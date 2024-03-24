37387fb4fe8655bc59f6a59137720b5b33fa55f9
ardiwayan470 <ardiwayan470@gmail.com>
2024-03-24 13:06:56 +0000

    commit vscode

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { Metadata } from 'next';
+import { Buku } from '@/data/buku';
 
 export const metadata: Metadata = {
     title: "home - wayan ardike",
@@ -7,10 +8,30 @@ export const metadata: Metadata = {
 };
 export default function Home() {
     return (
-        <div className='flex-col'>
-            <Link href={'/'} className=' flex'>
-                <h1>Hello, world!</h1>
-            </Link>
+        <div className='grid grid-cols-3 mobile:grid-cols-2 tablet:grid-cols-2 mini:grid-cols-1 gap-10 m-5'>
+            {Buku.map((item, index) => (
+                <ul key={index} className=' bg-slate-500 justify-center items-center rounded-lg flex-col p-3'>
+                    <li>
+                        {item.judul}
+                    </li>
+                    <li>
+                        {item.halaman}
+                    </li>
+                    <li>
+                        {item.penulis}
+                    </li>
+                    {item.Daftar.map((item, index) => (
+                        <ul key={index}>
+                            <li>
+                                {item.judul}
+                            </li>
+                            <li>
+                                {item.abstrak}
+                            </li>
+                        </ul>
+                    ))}
+                </ul>
+            ))}
         </div>
     );
 }
---
+++ src/data/buku.tsx
@@ -0,0 +1,56 @@
+export const Buku = [
+    {
+        'judul': 'Sumatera',
+        'halaman': 180,
+        'penulis': 'Dika',
+        'Daftar': [
+            {
+                'judul': 'Bab 1 pendahuluan',
+                'abstrak': 'pembukaan cerita'
+            },
+            {
+                'judul': 'Bab 1 pendahuluan',
+                'abstrak': 'pembukaan cerita'
+            },
+            {
+                'judul': 'Bab 1 pendahuluan',
+                'abstrak': 'pembukaan cerita'
+            }]
+    },
+    {
+        'judul': 'Sumatera',
+        'halaman': 180,
+        'penulis': 'Dika',
+        'Daftar': [
+            {
+                'judul': 'Bab 1 pendahuluan',
+                'abstrak': 'pembukaan cerita'
+            },
+            {
+                'judul': 'Bab 1 pendahuluan',
+                'abstrak': 'pembukaan cerita'
+            },
+            {
+                'judul': 'Bab 1 pendahuluan',
+                'abstrak': 'pembukaan cerita'
+            }]
+    },
+    {
+        'judul': 'Sumatera',
+        'halaman': 180,
+        'penulis': 'Dika',
+        'Daftar': [
+            {
+                'judul': 'Bab 1 pendahuluan',
+                'abstrak': 'pembukaan cerita'
+            },
+            {
+                'judul': 'Bab 1 pendahuluan',
+                'abstrak': 'pembukaan cerita'
+            },
+            {
+                'judul': 'Bab 1 pendahuluan',
+                'abstrak': 'pembukaan cerita'
+            }]
+    }
+]
